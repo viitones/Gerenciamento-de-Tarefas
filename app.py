e2d701717cf90c191cd15f79a9efa2eb680b1222
viitones <vitonesmartins@gmail.com>
2025-03-18 08:06:25 -0300

    Adiciona retorno do ID da tarefa criada e implementa testes para criação de tarefas

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def create_task():
   task_id_control += 1
   tasks.append(new_task)
   print(tasks)
-  return jsonify({"message": "Nova tarefa criada com sucesso"}), 201
+  return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id}), 201
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
