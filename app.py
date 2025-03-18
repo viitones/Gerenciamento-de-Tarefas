fb5e985eea667060f287d37007b3ff3c67118443
viitones <vitonesmartins@gmail.com>
2025-03-17 22:52:49 -0300

    Adiciona funcionalidade de criação de tarefas com Flask

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+tasks = []
+task_id_control = 1
 
-@app.route('/about')
-def about():
-  return 'The about page'
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  global task_id_control
+  data = request.get_json()
+  new_task = Task(id=task_id_control, title=data["title"], description=data.get("description", ""))
+  task_id_control += 1
+  tasks.append(new_task)
+  print(tasks)
+  return jsonify({"message": "Nova tarefa criada com sucesso"}), 201
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
